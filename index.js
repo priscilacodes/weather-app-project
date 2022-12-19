bd09140557755d9af3625057d4cd8f60f0c63b46
Pri-Codes <pri.codes@gmail.com>
2022-12-19 13:46:36 +1300

    Fixed the Forecast issue /new key

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5275fa016fd7058fa1d4233b4614b62d";
+  let apiKey = "bf54175800a55e59e6c4d6461deeef12";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
